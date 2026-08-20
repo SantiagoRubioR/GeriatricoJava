3657b052214aea230cb3ef496cc8b5647e6de38e
SantiagoRubioR <santiago.rubio.est@tecazuay.edu.ec>
2026-08-20 00:56:22 -0500

    VISTAS
    VISTAS COMPLETAS
    -login
    -dash enfermero
    -inicio enfermero
    -Pacientes-enfermero
    -Paciente_pantalla principal
    -Contacto de emergencia Paciente

---
+++ src/main/java/com/mycompany/geriatrico1/Geriatrico1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.geriatrico1;
+
+import com.mycompany.geriatrico1.vista.Dash_login;
+import com.mycompany.geriatrico1.vista.Dashboard_Enfermero;
+
+
+/**
+ *
+ * @author Santiago
+ */
+public class Geriatrico1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Dashboard_Enfermero d_Enf = new Dashboard_Enfermero();
+        Dash_login l = new Dash_login();
+        d_Enf.setVisible(true);
+        l.setVisible(false);
+        
+    }
+    
+}
