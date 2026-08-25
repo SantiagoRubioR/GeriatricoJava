6f3519ff7c66ad35b7c6df22711d26210a1f09c5
danielacorte <daniela.corte.est@tecazuay.edu.ec>
2026-08-25 02:03:52 -0500

    PUsuarioDao
    cambio en la estructura

---
+++ src/main/java/com/mycompany/geriatrico1/Controlador/Usuario.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.geriatrico1.Controlador;
+
+/**
+ *
+ * @author USUSRIO_ PC
+ */
+public class Usuario {
+
+    private String idUser;
+    private String nombreUser;
+    private String estadoUser;
+
+    private String idEmpleado;
+    private String cargoEmpleado;
+
+    private String cedulaPersona;
+    private String nombrePersona;
+    private String apellido1Persona;
+    private String apellido2Persona;
+
+    public Usuario() {
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNombreUser() {
+        return nombreUser;
+    }
+
+    public void setNombreUser(String nombreUser) {
+        this.nombreUser = nombreUser;
+    }
+
+    public String getEstadoUser() {
+        return estadoUser;
+    }
+
+    public void setEstadoUser(String estadoUser) {
+        this.estadoUser = estadoUser;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getCargoEmpleado() {
+        return cargoEmpleado;
+    }
+
+    public void setCargoEmpleado(String cargoEmpleado) {
+        this.cargoEmpleado = cargoEmpleado;
+    }
+
+    public String getCedulaPersona() {
+        return cedulaPersona;
+    }
+
+    public void setCedulaPersona(String cedulaPersona) {
+        this.cedulaPersona = cedulaPersona;
+    }
+
+    public String getNombrePersona() {
+        return nombrePersona;
+    }
+
+    public void setNombrePersona(String nombrePersona) {
+        this.nombrePersona = nombrePersona;
+    }
+
+    public String getApellido1Persona() {
+        return apellido1Persona;
+    }
+
+    public void setApellido1Persona(String apellido1Persona) {
+        this.apellido1Persona = apellido1Persona;
+    }
+
+    public String getApellido2Persona() {
+        return apellido2Persona;
+    }
+
+    public void setApellido2Persona(String apellido2Persona) {
+        this.apellido2Persona = apellido2Persona;
+    }
+
+    
+}
