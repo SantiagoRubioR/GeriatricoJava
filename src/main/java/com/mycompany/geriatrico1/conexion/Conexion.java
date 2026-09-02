297b50ffbdbbb8760a92e5491dbc453aeb80f94b
Mary-B2 <maria.bravo.est@tecazuay.edu.ec>
2026-09-02 11:09:42 -0500

    Eliminación de un botón innecesario
    Se elimino el botón horario de admin

--- src/main/java/com/mycompany/geriatrico1/conexion/Conexion.java
+++ src/main/java/com/mycompany/geriatrico1/conexion/Conexion.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 
 public class Conexion {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/sistemaGeriatricoV1.6.3";
+    private static final String URL = "jdbc:postgresql://localhost:5432/sistemaGeriatricoV6.3";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "admin";
+    private static final String PASSWORD = "luna";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
